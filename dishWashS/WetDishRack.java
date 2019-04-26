9730a4fa5b802f4b25842a9f2cd26dd8257986ef
Philip Amwata <philip@philipamwata.net>
2019-04-27 01:52:05 +0200

    Synchronization error, race condition occurs.

--- dishWashS/WetDishRack.java
+++ dishWashS/WetDishRack.java
@@ -4,38 +4,44 @@ import java.util.concurrent.Semaphore;
 
 public class WetDishRack {
 	private int rackSize;
-	private static int count = 1;
+	private static int count = 0;
 	private final Semaphore barrier;
+	private final Semaphore barrier2;
 
 	WetDishRack(int rackSize) {
 		this.rackSize = rackSize;
 		this.barrier = new Semaphore(0);
+		this.barrier2 = new Semaphore(1);
 		;
 	}
 
 	public void addDish(int dish_id) throws InterruptedException {
 		Semaphore sync = new Semaphore(1);
-		synchronized (sync) {
-			count = dish_id;
+		synchronized (barrier) {
 			if (count == rackSize) {
-				synchronized (barrier) {
-					barrier.wait();
+				barrier.wait();
+			} else {
+				synchronized (sync) {
+					count += 1;
+					barrier.notify();
 				}
 			}
 		}
-
 	}
 
 	public int removeDish() throws InterruptedException {
 		Semaphore sync = new Semaphore(1);
-		synchronized (sync) {
+
+		synchronized (barrier) {
 			if (count == 0) {
 				barrier.wait();
+			} else {
+				synchronized (sync) {
+					count -= 1;
+					barrier.notify();
+				}
 			}
-			synchronized (barrier) {
-				barrier.notify();
-				return count;
-			}
+			return count;
 		}
 	}
 }
