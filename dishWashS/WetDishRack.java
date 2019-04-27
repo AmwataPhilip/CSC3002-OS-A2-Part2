97f5bf55c4848ea7f365ab39a7f1570ab7b3e920
Philip Amwata <philip@philipamwata.net>
2019-04-27 12:02:00 +0200

    Changed code to include mutex, full barrier, empty barrier to provide full synchronization

--- dishWashS/WetDishRack.java
+++ dishWashS/WetDishRack.java
@@ -5,43 +5,33 @@ import java.util.concurrent.Semaphore;
 public class WetDishRack {
 	private int rackSize;
 	private static int count = 0;
-	private final Semaphore barrier;
-	private final Semaphore barrier2;
+	private final Semaphore sync;
+	private final Semaphore barrierFull;
+	private final Semaphore barrierEmpty;
 
 	WetDishRack(int rackSize) {
 		this.rackSize = rackSize;
-		this.barrier = new Semaphore(0);
-		this.barrier2 = new Semaphore(1);
+		this.barrierFull = new Semaphore(0);
+		this.barrierEmpty = new Semaphore(rackSize);
+		this.sync = new Semaphore(1);
 		;
 	}
 
 	public void addDish(int dish_id) throws InterruptedException {
-		Semaphore sync = new Semaphore(1);
-		synchronized (barrier) {
-			if (count == rackSize) {
-				barrier.wait();
-			} else {
-				synchronized (sync) {
-					count += 1;
-					barrier.notify();
-				}
-			}
-		}
+
+		barrierEmpty.acquire();
+		sync.acquire();
+		count = dish_id;
+		sync.release();
+		barrierFull.release();
 	}
 
 	public int removeDish() throws InterruptedException {
-		Semaphore sync = new Semaphore(1);
-
-		synchronized (barrier) {
-			if (count == 0) {
-				barrier.wait();
-			} else {
-				synchronized (sync) {
-					count -= 1;
-					barrier.notify();
-				}
-			}
-			return count;
-		}
+		barrierFull.acquire();
+		sync.acquire();
+		count -= 1;
+		sync.release();
+		barrierEmpty.release();
+		return count;
 	}
 }
