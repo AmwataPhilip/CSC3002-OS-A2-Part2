60e3cb3de7e7491895a0a235edaca9b7a83635c9
Philip Amwata <philip@philipamwata.net>
2019-04-27 12:27:45 +0200

    Synchronized the addDish and removeDish methods to avoid data races

--- dishWashS/WetDishRack.java
+++ dishWashS/WetDishRack.java
@@ -2,36 +2,49 @@ package dishWashS;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * Modified by: Philip Amwata
+ *
+ * date: 27/04/2019
+ */
 public class WetDishRack {
-	private int rackSize;
-	private static int count = 0;
-	private final Semaphore sync;
-	private final Semaphore barrierFull;
-	private final Semaphore barrierEmpty;
+	private static int count = 1; // Thread counter
+	private final Semaphore sync; // Mutex lock
+	private final Semaphore rackFull; // Semaphore to check if rack is not empty
+	private final Semaphore rackEmpty; // Semaphore to check if rack is not full
 
 	WetDishRack(int rackSize) {
-		this.rackSize = rackSize;
-		this.barrierFull = new Semaphore(0);
-		this.barrierEmpty = new Semaphore(rackSize);
+		this.rackFull = new Semaphore(0);
+		this.rackEmpty = new Semaphore(rackSize);
 		this.sync = new Semaphore(1);
 		;
 	}
 
+	/**
+	 *
+	 * @return void
+	 * @param dish_id
+	 * @throws InterruptedException
+	 */
 	public void addDish(int dish_id) throws InterruptedException {
-
-		barrierEmpty.acquire();
-		sync.acquire();
-		count = dish_id;
-		sync.release();
-		barrierFull.release();
+		rackEmpty.acquire(); // Rack is not full
+		sync.acquire(); // Lock rack for Washer
+		count = dish_id; // Ser washer dish to current dish id
+		sync.release(); // Unlock rack for dryer to use
+		rackFull.release(); // Rack is not empty
 	}
 
+	/**
+	 *
+	 * @return int
+	 * @throws InterruptedException
+	 */
 	public int removeDish() throws InterruptedException {
-		barrierFull.acquire();
-		sync.acquire();
-		count -= 1;
-		sync.release();
-		barrierEmpty.release();
-		return count;
+		rackFull.acquire(); // Rack is not empty
+		sync.acquire(); // Lock rack for Dryer
+		int number = count; // Set dryer dish to current dish id
+		sync.release(); // Unlock rack for washer to use
+		rackEmpty.release(); // Rack is not full
+		return number; // Return current dish id that dryer is working on
 	}
 }
